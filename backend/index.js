ad87923cfc28445e47953616dfa98957951eebb8
Cia de Condimentos Dev <developer@ciadecondimentos.com>
2026-05-21 13:17:31 -0300

    fix: Adicionar domínio ciadecondimento.netlify.app à lista de CORS permitidos

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,7 @@ const corsOptions = {
       'http://127.0.0.1:3000',
       'http://127.0.0.1:5000',
       'http://127.0.0.1:8080',
+      'https://ciadecondimento.netlify.app',
       'https://ciadecondimentosteste.netlify.app',
       'https://ciadecondimentos.netlify.app',
       'https://cia-de-condimentos.onrender.com',
